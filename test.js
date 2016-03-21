aa829a0e3c44dd4da4adc6d4cce03145986791b3
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-21 04:39:03 -0400

    run update

--- test.js
+++ test.js
@@ -11,13 +11,13 @@
 var should = require('should');
 var hasAny = require('./');
 
-describe('.hasAny()', function () {
-  it('should return `true` when the specified nested property exists.', function () {
+describe('.hasAny()', function() {
+  it('should return `true` when the specified nested property exists.', function() {
     hasAny({a: {b: {c: 'foo'}}}, ['a', 'g']).should.be.true;
     hasAny({a: {b: {c: 'foo'}}, bar: true}, ['foo', 'bar']).should.be.true;
   });
 
-  it('should return `false` when the specified nested property does not exist.', function () {
+  it('should return `false` when the specified nested property does not exist.', function() {
     hasAny({a: {b: {c: 'foo'}}}, ['b']).should.be.false;
     hasAny({a: {b: {c: 'foo'}}}, ['c']).should.be.false;
     hasAny({a: {b: {c: 'foo'}}}, ['d']).should.be.false;
